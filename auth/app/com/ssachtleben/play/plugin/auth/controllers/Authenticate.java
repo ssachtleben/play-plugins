ee422b67e1685d8f903e2894b57034fee111486a
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-08-01 01:17:54 +0200

    added javadoc

--- auth/app/com/ssachtleben/play/plugin/auth/controllers/Authenticate.java
+++ auth/app/com/ssachtleben/play/plugin/auth/controllers/Authenticate.java
@@ -6,18 +6,32 @@ import play.mvc.Result;
 import com.ssachtleben.play.plugin.auth.Auth;
 
 /**
- * Provides login and logout methods to handle authentication process. TODO: rework javadoc...
+ * Provides login and logout methods to handle authentication process via this plugin. The {@link #login(String)} and {@link #logout()}
+ * methods can directly linked in the routes configuration. But if you need additional code in this routes like jpa transaction, then you
+ * can create your own routes and use {@link Auth#login(play.mvc.Http.Context, String)} and {@link Auth#logout(play.mvc.Http.Session)}
+ * directly.
  * 
  * @author Sebastian Sachtleben
  */
 public class Authenticate extends Controller {
 
+	/**
+	 * Tries to login the current user with the given {@code provider}.
+	 * 
+	 * @param provider
+	 *          The provider key.
+	 * @return Play {@link Result} object.
+	 */
 	public static Result login(final String provider) {
 		return Auth.login(ctx(), provider);
 	}
 
+	/**
+	 * Logout the current logged in user.
+	 * 
+	 * @return Play {@link Result} object.
+	 */
 	public static Result logout() {
 		return Auth.logout(session());
 	}
-
 }
--- auth/app/com/ssachtleben/play/plugin/auth/exceptions/MissingConfigurationException.java
+++ auth/app/com/ssachtleben/play/plugin/auth/exceptions/MissingConfigurationException.java
@@ -1,10 +1,20 @@
 package com.ssachtleben.play.plugin.auth.exceptions;
 
+/**
+ * Throws if mandatory setting keys missing on provider plugin registration.
+ * 
+ * @author Sebastian Sachtleben
+ */
 @SuppressWarnings("serial")
 public class MissingConfigurationException extends Exception {
 
+	/**
+	 * Default constructor for {@link MissingConfigurationException}.
+	 * 
+	 * @param message
+	 *          The message to set.
+	 */
 	public MissingConfigurationException(String message) {
 		super(message);
 	}
-
 }
