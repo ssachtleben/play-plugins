cc30d2d1e50343b49f4188d31922d2dc8983f277
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-08-01 10:12:14 +0200

    added authenticates annotation

---
+++ auth/app/com/ssachtleben/play/plugin/auth/annotations/Authenticates.java
@@ -0,0 +1,33 @@
+package com.ssachtleben.play.plugin.auth.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.ssachtleben.play.plugin.auth.models.AuthUser;
+
+/**
+ * Authenticates specific {@link AuthUser} model.
+ * 
+ * @author Sebastian Sachtleben
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Authenticates {
+
+	/**
+	 * The AuthUser model which will be authenticated.
+	 * 
+	 * @return The cron pattern
+	 */
+	Class<? extends AuthUser> role() default AuthUser.class;
+
+	/**
+	 * The active boolean decides if the authentication method check will be used.
+	 * 
+	 * @return The active boolean
+	 */
+	boolean active() default true;
+
+}
