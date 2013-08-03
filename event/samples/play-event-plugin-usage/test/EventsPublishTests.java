7d0e85b4dae7a9382497325ca3280f627444d90c
Sebastian Sachtleben <ssachtleben@gmail.com>
2013-08-03 12:40:25 +0200

    added more publish tests

--- event/samples/play-event-plugin-usage/test/EventsPublishTests.java
+++ event/samples/play-event-plugin-usage/test/EventsPublishTests.java
@@ -20,13 +20,33 @@ public class EventsPublishTests extends EventTest {
 		assertThat(result.getReceivers()).hasSize(1);
 	}
 
+	@Test
+	public void publishTopicEvent() throws NoSuchMethodException, SecurityException {
+		events().unregisterAll();
+		events().register(TEST_TOPIC, getObserver("observeString", String.class));
+		EventResult result = events().publish(TEST_TOPIC, "Test");
+		assertThat(result.isPublished()).isTrue();
+		assertThat(result.getReceivers()).hasSize(1);
+	}
+
 	/**
 	 * Publishes an event without any subscriber...
 	 */
 	@Test
-	public void publishWithoutSubscriber() {
+	public void publishObjectEventWithoutSubscriber() {
 		events().unregisterAll();
 		EventResult result = events().publish("Test");
+		assertThat(result.isPublished()).isFalse();
+		assertThat(result.getReceivers()).hasSize(0);
+	}
+
+	/**
+	 * Publishes an event to a specific topic without any subscriber...
+	 */
+	@Test
+	public void publishTopicEventWithoutSubscriber() {
+		events().unregisterAll();
+		EventResult result = events().publish(TEST_TOPIC, "Test");
 		assertThat(result.isPublished()).isFalse();
 		assertThat(result.getReceivers()).hasSize(0);
 	}
