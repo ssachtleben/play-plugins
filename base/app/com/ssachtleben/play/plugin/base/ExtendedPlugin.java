9b2830659b89870c1440de11a432a6fb92b2ba70
Sebastian Sachtleben <ssachtleben@gmail.com>
2015-03-20 19:53:20 +0100

    added 3.0-SNAPSHOT for play 2.3

--- base/app/com/ssachtleben/play/plugin/base/ExtendedPlugin.java
+++ base/app/com/ssachtleben/play/plugin/base/ExtendedPlugin.java
@@ -1,78 +1,77 @@
 package com.ssachtleben.play.plugin.base;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import play.Application;
 import play.Logger;
 import play.Plugin;
 import play.libs.Akka;
+import scala.concurrent.duration.Duration;
 
 /**
  * @author Sebastian Sachtleben
  */
 public abstract class ExtendedPlugin extends Plugin {
 
-	/**
-	 * The {@link Logger} instance.
-	 */
-	protected final Logger.ALogger log = Logger.of(getClass());
+  /**
+   * The {@link Logger} instance.
+   */
+  protected final Logger.ALogger log = Logger.of(getClass());
 
-	/**
-	 * The {@link Application} instance.
-	 */
-	protected Application app;
+  /**
+   * The {@link Application} instance.
+   */
+  protected Application app;
 
-	/**
-	 * Default constructor for {@link ExtendedPlugin}.
-	 * 
-	 * @param app
-	 *          The {@link Application} instance.
-	 */
-	public ExtendedPlugin(final Application app) {
-		log.debug("Create Plugin");
-		this.app = app;
-	}
+  /**
+   * Default constructor for {@link ExtendedPlugin}.
+   * 
+   * @param app
+   *          The {@link Application} instance.
+   */
+  public ExtendedPlugin(final Application app) {
+    log.debug("Create Plugin");
+    this.app = app;
+  }
 
-	public abstract String name();
+  public abstract String name();
 
-	public abstract void start();
+  public abstract void start();
 
-	public abstract void stop();
+  public abstract void stop();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see play.Plugin#onStart()
-	 */
-	@Override
-	public void onStart() {
-		if (!app.configuration().getBoolean(String.format("%s.active", name()), Boolean.TRUE)) {
-			log.info(String.format("Plugin not started (Setting key '%s.active' is false)", name()));
-			return;
-		}
-		if (app.configuration().getBoolean(String.format("%s.async", name()), Boolean.TRUE)) {
-			log.debug("Start Plugin (async)");
-			Akka.future(new Callable<Void>() {
-				@Override
-				public Void call() throws Exception {
-					start();
-					return null;
-				}
-			});
-		} else {
-			log.debug("Start Plugin (sync)");
-			start();
-		}
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see play.Plugin#onStart()
+   */
+  @Override
+  public void onStart() {
+    if (!app.configuration().getBoolean(String.format("%s.active", name()), Boolean.TRUE)) {
+      log.info(String.format("Plugin not started (Setting key '%s.active' is false)", name()));
+      return;
+    }
+    if (app.configuration().getBoolean(String.format("%s.async", name()), Boolean.TRUE)) {
+      log.debug("Start Plugin (async)");
+      Akka.system().scheduler().scheduleOnce(Duration.create(0, TimeUnit.MILLISECONDS), new Runnable() {
+        public void run() {
+          start();
+        }
+      }, Akka.system().dispatcher());
+    } else {
+      log.debug("Start Plugin (sync)");
+      start();
+    }
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see play.Plugin#onStop()
-	 */
-	@Override
-	public void onStop() {
-		log.debug("Stop Plugin");
-		stop();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see play.Plugin#onStop()
+   */
+  @Override
+  public void onStop() {
+    log.debug("Stop Plugin");
+    stop();
+  }
 }
---
+++ base/build.sbt
@@ -0,0 +1,11 @@
+name := """play-base-plugin"""
+
+version := "3.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.11.1"
+
+organization := "com.ssachtleben"
+
+publishArtifact in(Compile, packageDoc) := false
