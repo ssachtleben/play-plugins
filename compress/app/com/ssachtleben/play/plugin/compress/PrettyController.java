bbedb64e25cd3a5ebcac681078cb2b8f62c4b308
Sebastian Sachtleben <ssachtleben@gmail.com>
2015-03-20 15:48:18 +0100

    added more implementations

--- compress/app/com/ssachtleben/play/plugin/compress/PrettyController.java
+++ compress/app/com/ssachtleben/play/plugin/compress/PrettyController.java
@@ -1,6 +1,7 @@
 package com.ssachtleben.play.plugin.compress;
 
 import play.Play;
+import play.api.templates.Html;
 import play.mvc.Content;
 import play.mvc.Controller;
 import play.mvc.Results;
@@ -8,47 +9,75 @@ import play.mvc.Results;
 import com.googlecode.htmlcompressor.compressor.HtmlCompressor;
 
 /**
+ * The PrettyController returns the {@link Content} or {@link Html} parameter
+ * for each important method e.g. ok, badRequest, notFound, forbidden,
+ * internalServerError and unauthorized prettified with yui compressor.
+ * 
  * @author Sebastian Sachtleben
  */
 public class PrettyController extends Controller {
 
-	public static Results.Status ok(Content content) {
+	public static Results.Status ok(final Content content) {
 		return Results.ok(prettify(content)).as("text/html; charset=utf-8");
 	}
 
-	public static Results.Status badRequest(Content content) {
+	public static Results.Status ok(final Html html) {
+		return Results.ok(prettify(html)).as("text/html; charset=utf-8");
+	}
+
+	public static Results.Status badRequest(final Content content) {
 		return Results.badRequest(prettify(content)).as("text/html; charset=utf-8");
 	}
 
-	public static Results.Status notFound(Content content) {
+	public static Results.Status badRequest(final Html html) {
+		return Results.badRequest(prettify(html)).as("text/html; charset=utf-8");
+	}
+
+	public static Results.Status notFound(final Content content) {
 		return Results.notFound(prettify(content)).as("text/html; charset=utf-8");
 	}
 
-	public static Results.Status forbidden(Content content) {
+	public static Results.Status notFound(final Html html) {
+		return Results.notFound(prettify(html)).as("text/html; charset=utf-8");
+	}
+
+	public static Results.Status forbidden(final Content content) {
 		return Results.forbidden(prettify(content)).as("text/html; charset=utf-8");
 	}
 
-	public static Results.Status internalServerError(Content content) {
+	public static Results.Status forbidden(final Html html) {
+		return Results.forbidden(prettify(html)).as("text/html; charset=utf-8");
+	}
+
+	public static Results.Status internalServerError(final Content content) {
 		return Results.internalServerError(prettify(content)).as("text/html; charset=utf-8");
 	}
 
-	public static Results.Status unauthorized(Content content) {
+	public static Results.Status internalServerError(final Html html) {
+		return Results.internalServerError(prettify(html)).as("text/html; charset=utf-8");
+	}
+
+	public static Results.Status unauthorized(final Content content) {
 		return Results.unauthorized(prettify(content)).as("text/html; charset=utf-8");
+	}
+
+	public static Results.Status unauthorized(final Html html) {
+		return Results.unauthorized(prettify(html)).as("text/html; charset=utf-8");
 	}
 
 	/**
 	 * Prettify the {@link Content}, e.g. removing whitespaces and linebreaks.
 	 * 
 	 * @param content
-	 *            The {@link Content} to prettify.
+	 *          The {@link Content} to prettify.
 	 * @return The prettified content.
 	 */
-	private static String prettify(Content content) {
+	private static String prettify(final Content content) {
 		if (Play.isDev()) {
 			return content.body();
 		}
-		HtmlCompressor compressor = new HtmlCompressor();
-		String output = content.body().trim();
+		final HtmlCompressor compressor = new HtmlCompressor();
+		final String output = content.body().trim();
 		compressor.setRemoveComments(true);
 		compressor.setCompressCss(true);
 		compressor.setCompressJavaScript(true);
